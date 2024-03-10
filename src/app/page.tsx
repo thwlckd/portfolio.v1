cd5e9755e41e9fd7736b4ebf6f88fa4fe3421f7a
thwlckd <thwlckd@naver.com>
2024-03-10 23:29:40 +0900

    Refactor: Sidebar 컴포넌트 layout 파일로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-'use client';
-
-import Link from 'next/link';
-import { Children } from 'react';
 import Home from '@/components/Home';
 import About from '@/components/About';
 import Skill from '@/components/Skill';
@@ -11,7 +7,6 @@ import Contact from '@/components/Contact';
 export default function Main() {
   return (
     <>
-      <SideBar />
       <Home />
       <About />
       <Skill />
@@ -20,25 +15,3 @@ export default function Main() {
     </>
   );
 }
-
-const SECTIONS = ['home', 'about', 'skill', 'project', 'contact'];
-
-const SideBar = () => {
-  return (
-    <ul className="fixed top-0 left-10 flex flex-col justify-center items-center gap-10 w-10 h-screen z-50">
-      {Children.toArray(
-        SECTIONS.map((section) => (
-          <li>
-            <Link
-              id={`${section}-bullet`}
-              className={
-                'block w-3 h-3 rounded-full bg-gray-500 opacity-50 transition-all duration-300'
-              }
-              href={`#${section}`}
-            ></Link>
-          </li>
-        )),
-      )}
-    </ul>
-  );
-};
