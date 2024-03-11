a0e8c5fd975660d85d07659d057c4e5882c3dfad
thwlckd <thwlckd@naver.com>
2024-03-11 15:32:59 +0900

    Style: Home 이름 애니메이션 작업

--- src/components/section/Home.tsx
+++ src/components/section/Home.tsx
@@ -2,15 +2,25 @@
 
 import { useRef } from 'react';
 import useRefObserver from '@/hooks/useRefObserver';
+import Link from 'next/link';
 
 export default function Home() {
   const homeRef = useRef(null);
 
   useRefObserver(homeRef);
-
   return (
-    <section id="home" className="min-h-screen" ref={homeRef}>
-      Home 이름애니메이션 배경
+    <section
+      id="home"
+      className="flex justify-center items-center min-h-screen"
+      ref={homeRef}
+    >
+      {/* Home 이름애니메이션 배경 */}
+      <Link className="text-6xl font-bold tracking-widest" href={'#about'}>
+        <span className="inline-block animate-spin-2">H</span>
+        <span>y</span>
+        <span>u</span>
+        <span className="inline-block animate-bounce">b</span>
+      </Link>
     </section>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,21 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        wiggle: 'wiggle 2s ease-in-out infinite',
+        'spin-2': 'spin-2 5s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-10deg)' },
+          '50%': { transform: 'rotate(10deg)' },
+        },
+        'spin-2': {
+          '0%, 80%': { transform: 'rotate(-1080deg)' },
+        },
+      },
+    },
   },
   plugins: [],
 };
