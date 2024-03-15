1474410218cdfd00aae054143c55a50031ad793a
thwlckd <thwlckd@naver.com>
2024-03-15 19:24:10 +0900

    Style: 프로젝트 카드 퍼블리싱

--- src/components/section/Project.tsx
+++ src/components/section/Project.tsx
@@ -1,7 +1,23 @@
 'use client';
 
-import { useRef } from 'react';
+import { Children, useRef } from 'react';
 import useRefObserver from '@/hooks/useRefObserver';
+import Image from 'next/image';
+
+const PROJECTS = [
+  {
+    name: 'Plantopia',
+    image: 'plantopia.png',
+  },
+  {
+    name: 'Fragrant',
+    image: 'fragrant.png',
+  },
+  {
+    name: 'Sfaclog',
+    image: 'sfaclog.png',
+  },
+];
 
 export default function Project() {
   const projectRef = useRef(null);
@@ -9,8 +25,25 @@ export default function Project() {
   useRefObserver(projectRef);
 
   return (
-    <section id="project" className="min-h-screen" ref={projectRef}>
-      project 프로젝트카드 모달
+    <section
+      id="project"
+      className="flex justify-center items-center flex-wrap gap-36 min-h-screen py-[20%] mr-20"
+      ref={projectRef}
+    >
+      {Children.toArray(
+        PROJECTS.map(({ name, image }) => (
+          <a className="flex flex-col items-center gap-4 hover:-translate-y-2 duration-300">
+            <Image
+              className="rounded-xl"
+              src={`/images/project/${image}`}
+              width={128}
+              height={128}
+              alt={name}
+            />
+            <p className="text-lg font-bold">{name}</p>
+          </a>
+        )),
+      )}
     </section>
   );
 }
