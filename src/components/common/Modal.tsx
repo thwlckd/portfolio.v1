22775c08b554c2bb70b1a7a3be0fca8098ce0792
thwlckd <thwlckd@naver.com>
2024-03-16 16:23:28 +0900

    Style: 모달 레이아웃 및 닫기 버튼 작업

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,5 +1,7 @@
 import { Dispatch, ReactNode, SetStateAction } from 'react';
 import { createPortal } from 'react-dom';
+import { motion } from 'framer-motion';
+import { SiIfixit } from 'react-icons/si';
 
 export interface ModalProps {
   isOpen: boolean;
@@ -27,14 +29,26 @@ const ModalUI = ({ setOpen, children }: ModalUIProps) => {
       className="absolute top-0 flex flex-col items-center w-full h-full backdrop-blur-md z-10"
       onClick={() => setOpen((prev) => !prev)}
     >
-      <main
+      <motion.a
+        className="sticky top-0 w-full h-[20dvh]"
+        initial={{ y: '-50%' }}
+        whileHover={{
+          y: 0,
+        }}
+      >
+        <SiIfixit className="w-full h-[50%] py-6 text-indigo-500" />
+      </motion.a>
+      <motion.main
         className={
-          'sticky top-[50vh] -translate-y-[50%] flex flex-col items-center min-w-[40dvw] bg-white shadow-lg h-[50dvh] '
+          'sticky top-[10dvh] flex flex-col items-center w-full h-[90dvh] bg-indigo-50 rounded-t-2xl overflow-y-scroll'
         }
+        initial={{ opacity: 0, y: '50%' }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ type: 'spring', damping: 15 }}
         onClick={(e) => e.stopPropagation()}
       >
         {children}
-      </main>
+      </motion.main>
     </div>
   );
 };
