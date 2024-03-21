e0f26628092b4e3122f528c01852c318154ba057
thwlckd <thwlckd@naver.com>
2024-03-21 15:45:22 +0900

    Style: Skill 섹션 반응형 작업

--- src/components/section/Skill.tsx
+++ src/components/section/Skill.tsx
@@ -25,67 +25,132 @@ import { cn } from '@/utils';
 
 const FRONT = [
   {
-    icon: <SiHtml5 size={50} color="white" />,
+    icon: (
+      <SiHtml5
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-orange-500',
     name: 'HTML5',
   },
   {
-    icon: <SiCss3 size={50} color="white" />,
+    icon: (
+      <SiCss3
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-sky-500',
     name: 'CSS3',
   },
   {
-    icon: <SiJavascript size={50} color="white" />,
+    icon: (
+      <SiJavascript
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-yellow-500',
     name: 'JavaScript',
   },
   {
-    icon: <SiTypescript size={50} color="white" />,
+    icon: (
+      <SiTypescript
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-blue-500',
     name: 'TypeScript',
   },
   {
-    icon: <SiReact size={50} color="white" />,
+    icon: (
+      <SiReact
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-cyan-500',
     name: 'React',
   },
   {
-    icon: <SiNextdotjs size={50} color="white" />,
+    icon: (
+      <SiNextdotjs
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-black',
     name: 'Next.js',
   },
   {
-    icon: <SiSass size={50} color="white" />,
+    icon: (
+      <SiSass
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-pink-500',
     name: 'Sass',
   },
   {
-    icon: <SiStyledcomponents size={50} color="white" />,
+    icon: (
+      <SiStyledcomponents
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-fuchsia-500',
     name: 'Styled Components',
   },
   {
-    icon: <SiTailwindcss size={50} color="white" />,
+    icon: (
+      <SiTailwindcss
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-teal-500',
     name: 'Tailwind',
   },
   {
-    icon: <SiRecoil size={50} color="white" />,
+    icon: (
+      <SiRecoil
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-blue-500',
     name: 'Recoil',
   },
   {
-    icon: <SiRedux size={50} color="white" />,
+    icon: (
+      <SiRedux
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-violet-500',
     name: 'Redux Toolkit',
   },
   {
-    icon: <SiReactquery size={50} color="white" />,
+    icon: (
+      <SiReactquery
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-red-500',
     name: 'React Query',
   },
   {
-    icon: <SiStorybook size={50} color="white" />,
+    icon: (
+      <SiStorybook
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-pink-600',
     name: 'Storybook',
   },
@@ -93,12 +158,22 @@ const FRONT = [
 
 const BACK = [
   {
-    icon: <SiNodedotjs size={50} color="white" />,
+    icon: (
+      <SiNodedotjs
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-lime-500',
     name: 'Node.js',
   },
   {
-    icon: <SiExpress size={50} color="white" />,
+    icon: (
+      <SiExpress
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-gray-500',
     name: 'Express.js',
   },
@@ -106,7 +181,12 @@ const BACK = [
 
 const ETC = [
   {
-    icon: <SiGit size={50} color="white" />,
+    icon: (
+      <SiGit
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
     backgroundColor: 'bg-orange-600',
     name: 'Git',
   },
@@ -136,11 +216,11 @@ export default function Skill() {
   return (
     <section
       id="skill"
-      className="relative flex flex-col justify-center items-center min-h-screen mr-[200px]"
+      className="relative flex flex-col justify-center items-center min-h-screen mr-[50px] sm:mr-[100px] lg:mr-[200px]"
       ref={skillRef}
     >
       <nav className="absolute top-[15%]">
-        <ul className="flex justify-center gap-20 text-2xl font-bold">
+        <ul className="flex justify-center gap-10 sm:gap-20 text-xl sm:text-2xl font-bold">
           {Children.toArray(
             FIELD.map(({ field, name }) => (
               <li className="relative" onClick={() => setCategory(() => field)}>
@@ -158,7 +238,7 @@ export default function Skill() {
         </ul>
       </nav>
       <motion.div
-        className="grid grid-cols-5 gap-10"
+        className="grid grid-cols-4 lg:grid-cols-5 gap-5 sm:gap-10"
         initial={{ x: '50dvw' }}
         whileInView={{
           x: 0,
@@ -167,6 +247,7 @@ export default function Skill() {
       >
         {category.map(({ icon, backgroundColor, name }) => (
           <motion.div
+            className="flex flex-col items-center"
             key={name}
             initial={{ opacity: 0, y: '200%' }}
             animate={{ opacity: 1, y: 0 }}
@@ -174,13 +255,15 @@ export default function Skill() {
           >
             <div
               className={cn(
-                'flex justify-center items-center w-20 h-20 rounded-full overflow-hidden hover:rotate-[360deg] transition-transform duration-1000',
+                'flex justify-center items-center w-[60px] h-[60px] sm:w-[70px] sm:h-[70px] lg:w-20 lg:h-20 rounded-full overflow-hidden hover:rotate-[360deg] transition-transform duration-1000',
                 backgroundColor,
               )}
             >
               {icon}
             </div>
-            <p className="w-20 mt-2 text-center leading-tight">{name}</p>
+            <p className="w-20 mt-2 text-sm sm:text-base text-center leading-tight">
+              {name}
+            </p>
           </motion.div>
         ))}
       </motion.div>
