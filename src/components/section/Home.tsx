d38c1ff9f0b0283c0fe70b2b14b0305d81174ac4
thwlckd <thwlckd@naver.com>
2024-03-21 12:36:27 +0900

    Style: Home 섹션 반응형 작업 - 모바일, 테블릿 유저를 고려한 nav tab 컬러 부여 - nav tab 배열 map 처리

--- src/components/section/Home.tsx
+++ src/components/section/Home.tsx
@@ -1,8 +1,32 @@
 'use client';
 
 import Link from 'next/link';
-import { useRef } from 'react';
+import { Children, useRef } from 'react';
 import useRefObserver from '@/hooks/useRefObserver';
+import { cn } from '@/utils';
+
+const NAVIGATORS = [
+  {
+    name: 'about',
+    color: 'text-red-300',
+    hover: 'hover:text-red-500',
+  },
+  {
+    name: 'skill',
+    color: 'text-orange-300',
+    hover: 'hover:text-orange-500',
+  },
+  {
+    name: 'project',
+    color: 'text-green-300',
+    hover: 'hover:text-green-500',
+  },
+  {
+    name: 'contact',
+    color: 'text-blue-300',
+    hover: 'hover:text-blue-500',
+  },
+];
 
 export default function Home() {
   const homeRef = useRef(null);
@@ -15,85 +39,29 @@ export default function Home() {
       className="relative min-h-screen bg-white z-10"
       ref={homeRef}
     >
-      <div className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] z-10 text-9xl font-bold tracking-widest transition-colors">
+      <div className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] z-10 text-5xl sm:text-8xl lg:text-9xl font-extrabold sm:font-bold tracking-widest transition-colors">
         <span className="inline-block animate-spin-2">H</span>
         <span>y</span>
         <span>u</span>
         <span className="inline-block animate-bounce">b</span>
       </div>
-      <nav className="max-h-screen max-w-screen overflow-hidden text-[250px] break-all leading-none text-white font-bold uppercase">
-        <Link
-          className="hover:text-red-500 transition-colors text-indigo-50"
-          href={'#about'}
-        >
-          about
-        </Link>
-        <Link
-          className="hover:text-orange-500 transition-colors text-indigo-50"
-          href={'#skill'}
-        >
-          skill
-        </Link>
-        <Link
-          className="hover:text-green-500 transition-colors text-indigo-50"
-          href={'#project'}
-        >
-          project
-        </Link>
-        <Link
-          className="hover:text-blue-500 transition-colors text-indigo-50"
-          href={'#contact'}
-        >
-          contact
-        </Link>
-        <Link
-          className="hover:text-red-500 transition-colors text-indigo-50"
-          href={'#about'}
-        >
-          about
-        </Link>
-        <Link
-          className="hover:text-orange-500 transition-colors text-indigo-50"
-          href={'#skill'}
-        >
-          skill
-        </Link>
-        <Link
-          className="hover:text-green-500 transition-colors text-indigo-50"
-          href={'#project'}
-        >
-          project
-        </Link>
-        <Link
-          className="hover:text-blue-500 transition-colors text-indigo-50"
-          href={'#contact'}
-        >
-          contact
-        </Link>
-        <Link
-          className="hover:text-red-500 transition-colors text-indigo-50"
-          href={'#about'}
-        >
-          about
-        </Link>
-        <Link
-          className="hover:text-orange-500 transition-colors text-indigo-50"
-          href={'#skill'}
-        >
-          skill
-        </Link>
-        <Link
-          className="hover:text-green-500 transition-colors text-indigo-50"
-          href={'#project'}
-        >
-          project
-        </Link>
-        <Link
-          className="hover:text-blue-500 transition-colors text-indigo-50"
-          href={'#contact'}
-        >
-          contact
-        </Link>
+      <nav className="max-h-screen max-w-screen overflow-hidden text-[120px] sm:text-[200px] lg:text-[250px] break-all leading-none text-white font-bold uppercase">
+        {Children.toArray(
+          Array.from({ length: 3 }).map(() =>
+            NAVIGATORS.map(({ name, color, hover }) => (
+              <Link
+                className={cn(
+                  'transition-colors text-indigo-50 lg:text-indigo-50',
+                  color,
+                  hover,
+                )}
+                href={`#${name}`}
+              >
+                {name}
+              </Link>
+            )),
+          ),
+        )}
       </nav>
     </section>
   );
