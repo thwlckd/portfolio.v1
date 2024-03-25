9e41a45060ec4769e5dc1c6ee30b75578ef06c37
thwlckd <thwlckd@naver.com>
2024-03-25 20:08:41 +0900

    Refactor: 상수 파일 분리

---
+++ src/constants/skill.tsx
@@ -0,0 +1,297 @@
+import {
+  SiAmazonaws,
+  SiCss3,
+  SiDocker,
+  SiExpress,
+  SiFirebase,
+  SiGit,
+  SiHtml5,
+  SiJavascript,
+  SiMongodb,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiPassport,
+  SiPostman,
+  SiReact,
+  SiRecoil,
+  SiReactquery,
+  SiRedux,
+  SiSass,
+  SiStorybook,
+  SiStyledcomponents,
+  SiTailwindcss,
+  SiTypescript,
+} from 'react-icons/si';
+
+export const FRONT = [
+  {
+    icon: (
+      <SiHtml5
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-orange-500',
+    name: 'HTML5',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiCss3
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-sky-500',
+    name: 'CSS3',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiJavascript
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-yellow-500',
+    name: 'JavaScript',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiTypescript
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-blue-500',
+    name: 'TypeScript',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiReact
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-cyan-500',
+    name: 'React',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiNextdotjs
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-black',
+    name: 'Next.js',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiSass
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-pink-500',
+    name: 'Sass',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiStyledcomponents
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-fuchsia-500',
+    name: 'Styled Components',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiTailwindcss
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-teal-500',
+    name: 'Tailwind',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiRecoil
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-blue-500',
+    name: 'Recoil',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiRedux
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-violet-500',
+    name: 'Redux Toolkit',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiReactquery
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-red-500',
+    name: 'React Query',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiStorybook
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-pink-600',
+    name: 'Storybook',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+];
+
+export const BACK = [
+  {
+    icon: (
+      <SiNodedotjs
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-lime-500',
+    name: 'Node.js',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiExpress
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-gray-500',
+    name: 'Express.js',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiPassport
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-black',
+    name: 'Passport.js',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiMongodb
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-green-500',
+    name: 'MongoDB',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiFirebase
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-amber-500',
+    name: 'Firebase',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiPostman
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-orange-500',
+    name: 'Postman',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+];
+
+export const ETC = [
+  {
+    icon: (
+      <SiGit
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-orange-600',
+    name: 'Git',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiAmazonaws
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-slate-800',
+    name: 'AWS',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+  {
+    icon: (
+      <SiDocker
+        className="text-[30px] sm:text-[40px] lg:text-[50px]"
+        color="white"
+      />
+    ),
+    backgroundColor: 'bg-blue-500',
+    name: 'Docker',
+    ability: 'after:content-["상"]',
+    description: '설명 블라블라',
+  },
+];
