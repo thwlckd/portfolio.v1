5a056f094f36cf621072cb2ef1ddae11376783de
thwlckd <thwlckd@naver.com>
2024-03-26 12:31:50 +0900

    Feat: 기술 스택 지표 설명 툴팁 작업

--- src/components/section/Skill.tsx
+++ src/components/section/Skill.tsx
@@ -2,10 +2,12 @@
 
 import { Children, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
+import { AiOutlineQuestionCircle } from 'react-icons/ai';
 import useRefObserver from '@/hooks/useRefObserver';
 import useCursor from '@/hooks/useCursor';
 import { cn } from '@/utils';
 import { BACK, ETC, FRONT } from '@/constants/skill';
+import Chip from '../common/Chip';
 
 const SKILLS = [
   { field: FRONT, name: 'Front' },
@@ -27,7 +29,7 @@ export default function Skill() {
       ref={skillRef}
     >
       <nav className="absolute top-[15dvh] z-10">
-        <ul className="flex justify-center gap-10 text-xl font-bold sm:gap-20 sm:text-2xl">
+        <ul className="relative flex justify-center gap-10 text-xl font-bold sm:gap-20 sm:text-2xl">
           {Children.toArray(
             SKILLS.map(({ field, name }) => (
               <li className="relative" onClick={() => setCategory(() => field)}>
@@ -42,10 +44,16 @@ export default function Skill() {
               </li>
             )),
           )}
+          <a className="group absolute -right-10 top-[50%] -translate-y-1/2 sm:-right-14 lg:-right-[120px]">
+            <AiOutlineQuestionCircle className="text-base sm:text-lg" />
+            <Chip className="invisible absolute -top-20 right-0 whitespace-pre-wrap text-nowrap text-start text-xs font-normal opacity-20 transition-opacity group-hover:visible group-hover:opacity-100 sm:text-sm">
+              {`1~3: 스닛펫을 참고하여 프로젝트에 사용\n4~6: 동작 메커니즘의 이해를 기반으로 프로젝트에 사용\n7~9: 능숙하게 프로젝트에 사용`}
+            </Chip>
+          </a>
         </ul>
       </nav>
       <motion.div
-        className="grid grid-cols-4 gap-5 sm:gap-10 lg:grid-cols-5"
+        className="relative grid grid-cols-4 gap-5 sm:gap-10 lg:grid-cols-5"
         initial={{ x: '50dvw' }}
         whileInView={{
           x: 0,
